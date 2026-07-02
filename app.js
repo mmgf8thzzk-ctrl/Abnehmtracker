2d14e00131e90a7c75728104d3c44c3fd6b3f29c
mmgf8thzzk-ctrl <mmgf8thzzk@privaterelay.appleid.com>
2026-07-02 21:41:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const gewicht = document.getElementById("gewicht");
+const ziel = document.getElementById("ziel");
 
+gewicht.addEventListener("input", () => {
+
+let rest = (gewicht.value - 82).toFixed(1);
+
+if(rest < 0){
+
+rest = 0;
+
+}
+
+ziel.innerHTML = rest + " kg";
+
+});
